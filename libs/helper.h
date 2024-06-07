6aee2100c89eaadf7e217c7d07f45857d0158da0
carlosega19 <cega2005@gmail.com>
2024-06-07 17:39:12 -0400

    Helper.h added (Functions and file managment)

---
+++ libs/helper.h
@@ -0,0 +1,217 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include "branch.h"
+#include "product.h"
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <iomanip>
+#include <conio.h>
+#include <windows.h>
+#include <locale.h>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+
+
+
+
+/*      FUNCTIONS HELPERS       */
+
+bool find(const char* arr, int size, char charc) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == charc) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void replaceTrash(string& line) {
+    char invalidChars[50] = "!@#$%^&*()_+=-/\\`~|'\".<:;[]{}?/";
+    int size = sizeof(invalidChars) - 1;
+
+    string newline;
+    for (char& c : line) {
+        if (!find(invalidChars, size, c)) {
+            newline += c;
+        }
+    }
+    line = newline;
+}
+
+
+/*---------------------------------*/
+//          FILE MANAGMENT
+void readBranchs(branch**B , branch**L , unordered_map<std::string, branch*>& tableB){ // Update the function to delete the trash in line
+    ifstream archivo("branchs.txt");
+    if (archivo.fail()) return; 
+    string n, s , c , a , t , code ,line;
+    while (getline(archivo , line))
+    {
+        replaceTrash(line);
+        istringstream ss(line);
+        string dato;
+        for (int i = 1; i < 7; i++)
+        {
+            getline(ss , dato , ',');
+            switch (i)
+            {
+                case 1:
+                    code = dato;
+                case 2:
+                    n = dato;
+                    break;
+                case 3:
+                    c = dato;
+                    break;
+                case 4:
+                    s = dato;
+                    break;
+                case 5:
+                    t=dato;
+                    break;
+                case 6:
+                    a=dato;
+                    break;
+            }
+        }
+        if (!tableB[code])
+        {
+            addBranch(B, L, code, n, c, s, a, t, tableB);
+        }
+    }
+    archivo.close();
+}
+
+// Write branchs in .txt
+void saveBranchs(branch*B){ 
+    ofstream archivo("branchs.txt");
+    if (archivo.is_open())
+    {
+        while (B)
+        {
+            archivo << B->code << "," << B->name << "," << B->city << "," << B->state << "," << B->tlf << "," << B->address << "\n";
+            B = B->next;
+        }
+    }
+    archivo.close();
+}
+
+// Take products of .txt
+void readProducts(product** P, product** L) { // Update the function to delete the trash in line
+    ifstream archivo("products.txt");
+    if (archivo.fail()) return;
+    string code, n, d,line;
+    while (getline(archivo, line))
+    {
+        replaceTrash(line);
+        istringstream ss(line);
+        string dato;
+
+        for (int i = 1; i < 4; i++)
+        {
+            getline(ss, dato, ',');
+            switch (i)
+            {
+                case 1:
+                    code = dato;
+                case 2:
+                    n = dato;
+                    break;
+                case 3:
+                    d = dato;
+                    break;
+            }
+        }
+        addProduct(P, L, code, n, d, 0, 0 , 0);
+    }
+    archivo.close();
+}
+// Write products in .txt
+void saveProducts(product* P) {
+    ofstream file("products.txt");
+    if (file.is_open())
+    {
+        while (P)
+        {
+            file << P->code << "," << P->name << "," << P->description << "\n";
+            P = P->next;
+        }
+    }
+    file.close();
+}
+
+// Take the products inside each branch
+void readProductsOfBranch(branch*B , product*P , unordered_map<std::string, branch*>& tableB){
+    ifstream archivo("inventory.txt");
+    if (archivo.fail()) return;
+    string codeB, codeP , line;
+    int am , minAm;
+    float price , temp;
+    branch*selectedB;
+    product*selectedP;
+    while (getline(archivo , line))
+    {
+        replaceTrash(line);
+        istringstream ss(line);
+        string data;
+        for (int i = 0; i < 5; i++)
+        {
+            getline(ss , data , ',');
+            switch (i)
+            {
+                case 0:
+                    codeB = data;
+                    break;
+                case 1:
+                    codeP = data;
+                    break;
+                case 2:
+                    am = stoi(data);
+                    break;
+                case 3:
+                    minAm = stoi(data);
+                    break;
+                case 4:
+                    price = stof(data);
+                    getline(ss, data, ',');
+                    temp = stof(data) / 100;
+                    price += temp;
+                    break;
+            }
+        }
+        selectedB = tableB[codeB];
+        selectedP = searchProductByCode(P , codeP);
+        if (selectedB && selectedP && am >= minAm && price > 0)
+        {
+            addProductToBranch(selectedB , selectedP , am , minAm, price);
+        }
+    }
+    archivo.close();
+}
+
+void saveProductsOfBranch(branch*B){
+    ofstream archivo("inventory.txt");
+    if (archivo.fail()) return;
+    while (B)
+    {
+        product*P = B->products; 
+        while (P)
+        {
+            double p = P->price;
+            double temp1;
+            double temp2 = modf(p, &temp1);
+            int temp3 = static_cast<int>(temp2 * 100);
+            archivo << B->code<<","<<P->code<<","<<P->amount<<","<<P->minAmount<<","<<temp1<<","<<temp3<<"\n";
+            P = P->next;
+        }
+        B = B->next;
+    }
+    archivo.close();
+}
+
+
+#endif // BRANCH_H
--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include "product.h"
-#include "branch.h"
-#include "menu.h"
+#include "libs/product.h"
+#include "libs/branch.h"
+#include "libs/menu.h"
 using namespace std;
 
 /*               FUNCTIONS               */
@@ -17,19 +17,148 @@ using namespace std;
 /*---------------------------------------*/
 
 
-int main()
-{
-    /*Variables*/
-    int op;
 
-    /*CODE HERE*/
-    do
-    {
-        system("cls");
-        menu();
-        cin >> op;
-    } while (op != 0);
-    return EXIT_SUCCESS;
+
+
+
+int main() {
+    setlocale(LC_ALL, "es_ES.UTF-8");
+	int op;
+    unordered_map<string, branch*> hashB;
+    branch* branchs = NULL;
+    branch* lastB = NULL;
+
+    product* products = NULL;
+    product* lastP = NULL;
+    // READ PRODUCTS
+    readProducts(&products, &lastP);
+    // READ BRANCHS
+    readBranchs(&branchs , &lastB , hashB);
+    // READ PRODUCTS INSIDE BRANCHS
+    readProductsOfBranch(branchs , products , hashB);
+	do
+	{
+		menu();
+		cin >> op;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore();
+            op = -1;
+        }
+        fflush(stdin);
+		switch (op)
+		{
+			case 1: // Mantenimiento
+				do
+				{
+					menuMant();
+					cin >> op;
+                    if (cin.fail())
+                    {
+                        cin.clear();
+                        cin.ignore();
+                        op = -1;
+                    }
+					switch (op)
+					{
+						case 1: // Mantenimiento Productos
+                            do
+                            {
+                                menuMantProds();
+                                cin >> op;
+                                if (cin.fail())
+                                {
+                                    cin.clear();
+                                    cin.ignore();
+                                    op = -1;
+                                }
+                                switch (op)
+                                {
+                                    case 1: // Add Product
+                                        createProduct(&products, &lastP);
+                                        break;
+                                    case 2: // MODIFY A PRODUCT
+                                        menuModProduct(products);
+                                        break;
+                                    case 3: // DELETE A PRODUCT
+                                        menuDelProduct(&products , &lastP);
+                                        break;
+                                    case 4:
+                                        menuConsultProductByCode(products);
+                                        break;
+                                    case 5:
+                                        menuConsultProductByDesc(products);
+                                        break;
+                                    case 6:
+                                        //showAllProducts(products);
+                                        tableProducts(products);
+                                        system("pause");
+                                        break;
+                                }
+                            } while (op != 0);
+                            op = -1;
+							break;
+						case 2: // Mantenimiento Sucursales
+							do
+                            {
+                                fflush(stdin);
+                                menuMantBranchs();
+                                cin >> op;
+                                if (cin.fail())
+                                {
+                                    cin.clear();
+                                    cin.ignore();
+                                    op = -1;
+                                }
+                                switch (op)
+                                {
+                                    case 1: // Add Branch
+                                        createBranch(&branchs , &lastB , hashB);
+                                        break;
+                                    case 2: // Modify Branch
+                                        menuModBranch(branchs);
+                                        break;
+                                    case 3: // Delete Branch
+                                        menuDeleBranch(&branchs , &lastB);
+                                        break;
+                                    case 4: // Consult By Code
+                                        menuConsultBranchByCode(branchs);
+                                        break;
+                                    case 5: // Consult By Description
+                                        menuConsultBranchByDesc(branchs);
+                                        break;
+                                    case 6: // Show All Branchs  
+                                        //printBranchs(branchs);
+                                        tableBranchs(branchs);
+                                        system("pause");
+                                        break;
+                                    case 7: // INVENTORY
+                                        menuInventory(branchs , products);
+                                        break;
+                                }
+                            } while (op != 0);
+                            op = -1;
+							break;
+					}
+					/* ---------- FIN MANTENIMIENTO ---------- */
+				} while (op != 0);
+				op = -1;
+				break;
+            case 2:
+            {
+                // No available
+                break;
+            }
+			case 3:
+				// No available
+				break;
+		}
+	} while (op != 0);
+    saveProducts(products);
+	saveBranchs(branchs);
+    saveProductsOfBranch(branchs);
+    return 0;
 }
 
 // Ahora si funciona
