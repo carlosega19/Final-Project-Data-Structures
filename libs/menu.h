801f4ed78ce748695bf9ec7036b6bb1d1169929f
carlosega19 <cega2005@gmail.com>
2024-06-15 12:51:43 -0400

    Limpiando menuns

--- libs/menu.h
+++ libs/menu.h
@@ -27,7 +27,7 @@ mensajeInformativo *MI() {
 
 static mensajeInformativo *Mensajero = MI();
 const string NUM_VALIDO = "1234567890";
-const int NS = 10; /* ver si los string son terminados en NULL para eliminar este campo*/
+const int NS = 10; /* ver si los string son terminados en NULL para eliminar este campo */
 
 
 void actualizarMensaje(string m) {
@@ -47,78 +47,6 @@ void menuHeader() {
     cout << line << endl;
 }
 
-// MAIN MENU
-void menu() {
-	system("cls");
-	// Ancho total deseado de la l?nea
-	string subtitle = "MENU PRINCIPAL";
-	string op1 = "1. MANTENIMIENTO";
-	string op2 = "2. FACTURACION";
-	string op3 = "3. REPORTES";
-	string op0 = "0. SALIR.";
-	string msg = "Su opcion (0-3) : _|";
-
-	menuHeader();
-	cout << setw((lineWidth + length(subtitle)) / 2) << subtitle << endl;
-	cout << line << endl;
-	cout << setw((lineWidth + length(op1)) / 2) << op1 << endl;
-	cout << setw((lineWidth + length(op2)) / 2) << op2 << endl;
-	cout << setw((lineWidth + length(op3)) / 2) << op3 << endl;
-	cout << setw((lineWidth + length(op0)) / 2) << op0 << endl;
-	cout << setw((lineWidth + length(msg)) / 2) << msg << endl;
-	cout << line << endl;
-	printCreators();
-}
-
-// MANTENIENCE
-void menuMant() {
-	system("cls");
-	menuHeader();
-	string subtitle = "1. MANTENIMIENTO";
-	string op1 = "1. PRODUCTOS";
-	string op2 = "2. SUCURSALES";
-	string op3 = "3. PERSONAS";
-	string op0 = "0. VOLVER.";
-	string msg = "Su opcion (0-3) : _|";
-
-	cout << setw((lineWidth + length(subtitle)) / 2) << subtitle << endl;
-	cout << line << endl;
-	cout << setw((lineWidth + length(op1)) / 2) << op1 << endl;
-	cout << setw((lineWidth + length(op2)) / 2) << op2 << endl;
-	cout << setw((lineWidth + length(op3)) / 2) << op3 << endl;
-	cout << setw((lineWidth + length(op0)) / 2) << op0 << endl;
-	cout << setw((lineWidth + length(msg)) / 2) << msg << endl;
-	cout << line << endl;
-}
-
-// MATENIENCE__PRODUCTS
-void menuMantProds() {
-	system("cls");
-	menuHeader();
-
-	string subtitle = "1.1 MANTENIMIENTO PRODUCTOS";
-	string op1 = "1.1. 1. AGREGAR";
-	string op2 = "1.1. 2. MODIFICAR";
-	string op3 = "1.1. 3. ELIMINAR";
-	string op4 = "1.1. 4. CONSULTA POR CODIGO";
-	string op5 = "1.1. 5. CONSULTA POR DESCRIPCION";
-	string op6 = "1.1. 6. MOSTRAR TODOS LOS PRODUCTOS";
-	string op0 = "0. VOLVER A MENU ANTERIOR.";
-	string msg = "Su opcion (0-6) : _|";
-
-
-	cout << setw((lineWidth + length(subtitle)) / 2) << subtitle << endl;
-	cout << line << endl;
-	cout << setw((lineWidth + length(op1)) / 2) << op1 << endl;
-	cout << setw((lineWidth + length(op2)) / 2) << op2 << endl;
-	cout << setw((lineWidth + length(op3)) / 2) << op3 << endl;
-	cout << setw((lineWidth + length(op4)) / 2) << op4 << endl;
-	cout << setw((lineWidth + length(op5)) / 2) << op5 << endl;
-	cout << setw((lineWidth + length(op6)) / 2) << op6 << endl;
-	cout << setw((lineWidth + length(op0)) / 2) << op0 << endl;
-	cout << setw((lineWidth + length(msg)) / 2) << msg << endl;
-	cout << line << endl;
-}
 
 void optionsModProduct(product*selected) {
     system("cls");
