aef1ee9ef5f7579ae069fb27337f28b0e6322741
carlosega19 <cega2005@gmail.com>
2024-06-21 22:23:48 -0400

    Facturas doblemente enlazadas dipolo

--- libs/bill.cpp
+++ libs/bill.cpp
@@ -20,7 +20,6 @@ struct details
 
 struct bill
 {
-    int BF;
     string code;
     string clientId;
     string date;
@@ -28,9 +27,17 @@ struct bill
     details *detailBill;
     
     /* POINTERS */
-    bill*left;
-    bill*right;
+    bill *next;
+    bill *prev;
 };
+
+//  Idea (hacer un dipolo?)
+struct dipolo
+{
+    bill*first;
+    bill*last;
+};
+
 
 bill *newBill(string num, string cId, string date, float t) {
     bill *nbill = new bill;
@@ -38,23 +45,98 @@ bill *newBill(string num, string cId, string date, float t) {
     nbill->date = date;
     nbill->clientId = cId;
     nbill->total = t;
-
-    /* DEFAULT */
-    nbill->BF = 0;
-    nbill->left = NULL;
-    nbill->right = NULL;
-}
-int main() {
-    bill *a = NULL;
     
-    addBill(&a,"18","","",0);
-    addBill(&a,"16","","",0);
-    addBill(&a,"20","","",0);
-    inorden(a);
+    nbill->next = NULL;
+    nbill->prev = NULL;
+    return nbill;
+}
 
+int compare(string num1, string num2) {
+    return stoi(num1) > stoi(num2);
+}
+
+int isEqual(string num1, string num2) {
+    return stoi(num1) == stoi(num2);
+}
+
+// Insertar ordenadamente por codigo de factura
+bool addBill(dipolo**P, string num, string cId, string date, float t) {
+    if (!(*P)->first || compare((*P)->first->code, num))
+    {
+        if ((*P)->first && (*P)->first->code == num) return false;
+        bill *newB = newBill(num, cId, date, t);
+
+        newB->next = (*P)->first;
+        if ((*P)->first) (*P)->first->prev = newB;
+        else (*P)->last = newB;
+        (*P)->first = newB;
+    }
+    else {
+        bill *ax = (*P)->first;
+        while (ax->next && compare(num, ax->next->code)) {            
+            ax = ax->next;
+        }
+        if (ax->next && ax->next->code == num) return false;
+        else {
+            bill *newB = newBill(num, cId, date, t);
+            newB->next = ax->next;
+            newB->prev = ax;
+            if (ax->next) ax->next->prev = newB;
+            ax->next = newB;
+            if(ax == (*P)->last) (*P)->last = newB;
+        }
+    }
+    return true;
+}
+
+// Eliminar una factura
+void deleteBill(dipolo**P, string num) {
+    bill *ax = (*P)->first;
+    while (ax && !isEqual(ax->code, num)) {
+        ax = ax->next;
+    }
+
+    if (ax) {
+        bill *t = ax;
+        if (ax == (*P)->first) (*P)->first = (*P)->first->next;
+        if (ax == (*P)->last) (*P)->last = (*P)->last->prev; 
+        if (ax->prev) ax->prev->next = ax->next;
+        if (ax->next) ax->next->prev = ax->prev;
+        delete t;
+    }
+}
+
+// Funcion de uso para pruebas
+void printBills(bill*B) {
+    while (B)
+    {
+        cout << B->code << endl;
+        B = B->next;
+    }
+}
+
+
+int main() {
+    dipolo *bills = new dipolo;
+    bills->first = NULL;
+    bills->last = NULL;
+    addBill(&bills, "10", "", "",0);
+    addBill(&bills, "1", "", "",0);
+    addBill(&bills, "5", "", "",0);
+    addBill(&bills, "2", "", "",0);
+    addBill(&bills, "2", "", "",0);
+    addBill(&bills, "20", "", "",0);
+    printBills(bills->first);
+    
+    cout << "\n\n";
     system("pause");
-    deleteBill(&a, "18");
-    inorden(a);
+    cout << "\n\n";
+
+    deleteBill(&bills, "1");
+    printBills(bills->first);
+
+
+    cout << "\n\n";
     system("pause");
     return 0;
 }
